19f65de4e4c4f270e0f650055bedc0cc7f9fe263
NatawaSergeevna <nett0daynet@gmail.com>
2023-08-17 11:13:49 +0400

    new file 1

---
+++ d1.py
@@ -0,0 +1,22 @@
+class Item:
+    #конструктор
+
+    #сложение
+
+    #вычитание
+
+    #умножение
+
+    #деление
+    
+
+class Item2:
+
+
+item1 = Item(5)
+item2 = Item2(0)
+
+print()
+print()
+print()
+print()
